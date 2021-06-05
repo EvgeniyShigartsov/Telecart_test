f1b621c97033cf93eaba9012159a27096a408b3f
Evgeniy Shigartsov <evgeniy.shigartsov2@gmail.com>
2021-06-05 14:18:28 +0300

    set environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css';
+import React, { FC } from 'react';
 
-function App() {
-  return (
-    <div className="app">
-      <h1>Hello world</h1>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="app">
+    <h1>Hello world</h1>
+  </div>
+);
 
 export default App;
